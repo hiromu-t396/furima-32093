48f36f49eed9c9307d143a2be61a9975d8c43697
hiromu-t396 <hiromu1193966@gmail.com>
2020-11-19 17:33:21 +0900

    with_optionsを使いpresenceをまとめた

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,14 +2,24 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :nickname, presence: true
   validates :email, uniqueness: true
-  validates :password,format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i }
-  validates :lastname, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]\z/ }
-  validates :firstname, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]\z/ }
-  validates :lastname_kana, presence: true, format: { with: /\A[ァ-ン]\z/ }
-  validates :lastname_kana, presence: true, format: { with: /\A[ァ-ン]\z/ }
+  validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i }
+  validates :lastname, format: { with: /\A[ぁ-んァ-ン一-龥]\z/ }
+  validates :firstname, format: { with: /\A[ぁ-んァ-ン一-龥]\z/ }
+  validates :lastname_kana, format: { with: /\A[ァ-ン]\z/ }
+  validates :firstname_kana, format: { with: /\A[ァ-ン]\z/ }
   validates :birth_day, presence: true
   has_many :items
   has_many :transactions
+
+  with_options presence true do
+    validates :nickname
+    validates :password
+    validates :lastname
+    validates :firstname
+    validates :lastname_kana
+    validates :firstname_kana
+    validates :birth_day
+  end
+  
 end
