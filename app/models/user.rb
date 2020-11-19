92c4d21654ea202c35f63754b20973043b4c602a
hiromu-t396 <hiromu1193966@gmail.com>
2020-11-19 10:36:11 +0900

    各バリデーション記述

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,15 @@
 class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :nickname, presence: true
+  validates :email, presence: true, unique: true, format: { with: /\A[@]\z/ }
+  validates :encrypted_password, length: {minimum: 6 }, presence: true, format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i}
+  validates :lastname, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]\z/}
+  validates :firstname, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]\z/}
+  validates :lastname_kana, presence: true, format: { with: /\A[ァ-ン]\z/}     
+  validates :lastname_kana, presence: true, format: { with: /\A[ァ-ン]\z/}
+  validates :birth_day, presence: true
+  has_many :items
+  has_many :transactions
 end
