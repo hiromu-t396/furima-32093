81eccce6801c5512abaf70221130c33f9457218c
hiromu-t396 <hiromu1193966@gmail.com>
2020-11-19 14:29:25 +0900

    rubocopで修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,12 +3,12 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :nickname, presence: true
-  validates :email, presence: true, uniqueness: true, format: { with: /\A[@]\z/ }
-  validates :encrypted_password, length: {minimum: 6 }, presence: true, format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i}
-  validates :lastname, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]\z/}
-  validates :firstname, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]\z/}
-  validates :lastname_kana, presence: true, format: { with: /\A[ァ-ン]\z/}     
-  validates :lastname_kana, presence: true, format: { with: /\A[ァ-ン]\z/}
+  validates :email, presence: true, uniqueness: true, format: { with: /\A@\z/ }
+  validates :encrypted_password, length: { minimum: 6 }, presence: true, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i }
+  validates :lastname, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]\z/ }
+  validates :firstname, presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]\z/ }
+  validates :lastname_kana, presence: true, format: { with: /\A[ァ-ン]\z/ }
+  validates :lastname_kana, presence: true, format: { with: /\A[ァ-ン]\z/ }
   validates :birth_day, presence: true
   has_many :items
   has_many :transactions
