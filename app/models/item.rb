5e413283ef9f48ddb6d224e9069c735d5a693df7
hiromu-t396 <hiromu1193966@gmail.com>
2020-11-21 17:42:42 +0900

    prefectureモデル作成

--- app/models/item.rb
+++ app/models/item.rb
@@ -21,6 +21,9 @@ class Item < ApplicationRecord
   belongs_to :condition
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :delivery
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :prefecture
+
   has_one_attached :image
   belongs_to :user
   has_one :transaction
