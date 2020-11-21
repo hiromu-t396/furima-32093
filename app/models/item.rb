546b7ff0fa8397cd9a9f75288fa32b22955ad3fa
hiromu-t396 <hiromu1193966@gmail.com>
2020-11-21 12:57:55 +0900

    categoryモデル作成とバリデーション

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,22 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+
+  validates :name, presence: true
+  validates :explanation, presence: true
+  validates :category_id, presence: true
+  validates :condition_id, presence: true
+  validates :delivery_id, presence: true
+  validates :prefecture_id, presence: true
+  validates :delivery_days_id, presence: true
+  validates :price, presence: true
+
+  validates :category_id, numericality: { other_than: 1 }
+  validates :condition_id, numericality: { other_than: 1 }
+  validates :delivery_id, numericality: { other_than: 1 }
+  validates :prefecture_id, numericality: { other_than: 1 }
+  validates :delivery_days_id, numericality: { other_than: 1 }
+
   has_one_attached :image
   belongs_to :user
   has_one :transaction
