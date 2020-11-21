d442d4890aae319e66ac21b216e1e808cd08848e
hiromu-t396 <hiromu1193966@gmail.com>
2020-11-21 11:40:14 +0900

    cotrollerにpermitの記述

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -4,6 +4,13 @@ class ItemsController < ApplicationController
   end
 
   def new
-    
+
   end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:image, :name, :explanation, :category_id, :condition_id, :delivery_id, :prefecture_id, :delivery_days_id, :price).merge(user_id: current_user.id)
+  end
+
 end
