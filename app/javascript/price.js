8e1fa24d3439d63fca954beddfbf2b81280d49fc
hiromu-t396 <hiromu1193966@gmail.com>
2020-11-23 12:32:12 +0900

    javascript手数料、利益実装

--- app/javascript/price.js
+++ app/javascript/price.js
@@ -1,3 +1,15 @@
 window.addEventListener('load', () => {
   console.log("OK");
+  const priceInput = document.getElementById("item-price");
+  priceInput.addEventListener("input", () => {
+   const inputValue = priceInput.value;
+   const addTaxDom = document.getElementById("add-tax-price");
+    addTaxDom.innerHTML = Math.floor(inputValue*0.1);
+    console.log("Math.floor",addTaxDom);
+    const addProfitDom = document.getElementById("profit");
+    addProfitDom.innerHTML = (inputValue-addTaxDom.innerHTML);
+    console.log(addProfitDom);
+})
 });
+
+
