5de16b6bc252460389ae4a2602def0abfcd79c18
hiromu-t396 <hiromu1193966@gmail.com>
2020-11-24 19:34:05 +0900

    表示をidの大きい順に表示

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -49,7 +49,7 @@ RSpec.describe Item, type: :model do
         expect(@item.errors.full_messages).to include('Delivery day must be other than 1')
       end
       it 'priceが9,999,999以上だと登録できない' do
-        @item.price = 10000000
+        @item.price = 10_000_000
         @item.valid?
         expect(@item.errors.full_messages).to include('Price must be less than or equal to 9999999')
       end
