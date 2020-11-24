8b4a1b448c9cfd0275bf968c0fbb7e5754028535
hiromu-t396 <hiromu1193966@gmail.com>
2020-11-24 10:26:55 +0900

    投稿がうまく行く時のテストコード

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,5 +1,41 @@
 require 'rails_helper'
 
 RSpec.describe Item, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @item = FactoryBot.build(:item)
+  end
+
+  describe '商品投稿機能' do
+    context '商品投稿がうまく行く時' do
+      it 'name, explanation, category_id, condition_id, delivery_id, prefecture_id, delivery_day_id, price, imageが存在すれば登録できる' do
+       expect(@item).to be_valid
+      end 
+    end
+
+    context '商品投稿がうまくいかない時' do
+      it 'nameが空だと登録できない' do
+        @item.name = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Name can't be blank")
+      end
+      it 'explanationが空だと登録できない' do
+        @item.explanation = ''
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Explanation can't be blank")
+      end
+      it 'category_idが1を選択していると登録できない' do
+        @item.category_id = '1'
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Category must be other than 1")
+      end
+      it 'condition_idが1を選択していると登録できない' do
+        @item.condition_id = '1'
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Condition must be other than 1")
+      end
+    end
+  end
 end
+
+
+
