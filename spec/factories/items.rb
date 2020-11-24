70f1442be3446c46c39b2a18a8c779bd91dbf655
hiromu-t396 <hiromu1193966@gmail.com>
2020-11-24 10:28:08 +0900

    カテゴリーの部分を修正

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,17 @@
 FactoryBot.define do
   factory :item do
+    name                  { Faker::Lorem.characters(number: 10) }
+    explanation           { Faker::Lorem.characters(number: 10) }
+    category_id           { 2 }
+    condition_id          { 2 }
+    delivery_id           { 2 }
+    prefecture_id         { 2 }
+    delivery_day_id       { 2 }
+    price                 { 1000 }
+    association :user
     
+    after(:build) do |item|
+      item.image.attach(io: File.open('public/images/test_image.jpg'), filename: 'test_image.jpg')
+    end
   end
 end
