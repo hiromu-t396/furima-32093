35fca24d53ba48a2d2d771bec89d07c83c0f15b9
hiromu-t396 <hiromu1193966@gmail.com>
2020-11-24 12:05:47 +0900

    console.log削除

--- app/javascript/price.js
+++ app/javascript/price.js
@@ -5,10 +5,8 @@ window.addEventListener('load', () => {
    const inputValue = priceInput.value;
    const addTaxDom = document.getElementById("add-tax-price");
     addTaxDom.innerHTML = Math.floor(inputValue*0.1);
-    // console.log("Math.floor",addTaxDom);
     const addProfitDom = document.getElementById("profit");
     addProfitDom.innerHTML = (inputValue-addTaxDom.innerHTML);
-    // console.log(addProfitDom);
 })
 });
 
