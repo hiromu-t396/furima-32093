a5c9665c3be339cf926ee1527f5521f95756be32
hiromu-t396 <hiromu1193966@gmail.com>
2020-11-24 11:13:56 +0900

    modelをwith_optionsを使いまとめた

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -8,8 +8,8 @@ RSpec.describe Item, type: :model do
   describe '商品投稿機能' do
     context '商品投稿がうまく行く時' do
       it 'name, explanation, category_id, condition_id, delivery_id, prefecture_id, delivery_day_id, price, imageが存在すれば登録できる' do
-       expect(@item).to be_valid
-      end 
+        expect(@item).to be_valid
+      end
     end
 
     context '商品投稿がうまくいかない時' do
@@ -26,37 +26,42 @@ RSpec.describe Item, type: :model do
       it 'category_idが1を選択していると登録できない' do
         @item.category_id = 1
         @item.valid?
-        expect(@item.errors.full_messages).to include("Category must be other than 1")
+        expect(@item.errors.full_messages).to include('Category must be other than 1')
       end
       it 'condition_idが1を選択していると登録できない' do
         @item.condition_id = 1
         @item.valid?
-        expect(@item.errors.full_messages).to include("Condition must be other than 1")
+        expect(@item.errors.full_messages).to include('Condition must be other than 1')
       end
       it 'delivery_idが1を選択していると登録できない' do
         @item.delivery_id = 1
         @item.valid?
-        expect(@item.errors.full_messages).to include("Delivery must be other than 1")
+        expect(@item.errors.full_messages).to include('Delivery must be other than 1')
       end
       it 'prefecture_idが1を選択していると登録できない' do
         @item.prefecture_id = 1
         @item.valid?
-        expect(@item.errors.full_messages).to include("Prefecture must be other than 1")
+        expect(@item.errors.full_messages).to include('Prefecture must be other than 1')
       end
       it 'delivery_day_idが1を選択していると登録できない' do
         @item.delivery_day_id = 1
         @item.valid?
-        expect(@item.errors.full_messages).to include("Delivery day must be other than 1")
+        expect(@item.errors.full_messages).to include('Delivery day must be other than 1')
       end
       it 'priceが9,999,999以上だと登録できない' do
-        @item.price = 10000000
+        @item.price = 10_000_000
         @item.valid?
-        expect(@item.errors.full_messages).to include("Price must be less than or equal to 9999999")
+        expect(@item.errors.full_messages).to include('Price must be less than or equal to 9999999')
       end
       it 'priceが300以下だと登録できない' do
         @item.price = 299
         @item.valid?
-        expect(@item.errors.full_messages).to include("Price must be greater than or equal to 300")
+        expect(@item.errors.full_messages).to include('Price must be greater than or equal to 300')
+      end
+      it 'priceが半角数字以外だと登録できない' do
+        @item.price = '３００'
+        @item.valid?
+        expect(@item.errors.full_messages).to include('Price is not a number')
       end
       it 'imageが空だと登録できない' do
         @item.image = nil
@@ -66,6 +71,3 @@ RSpec.describe Item, type: :model do
     end
   end
 end
-
-
-
