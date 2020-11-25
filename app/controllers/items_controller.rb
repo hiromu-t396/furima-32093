b53e4b4bb2df7fd8d003b625c0ae25507dbaaaa9
hiromu-t396 <hiromu1193966@gmail.com>
2020-11-25 19:45:54 +0900

    @item = Item.find(params[:id])をプライベートにまとめて記述

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new]
+  before_action :set_item, only: [:show, :edit, :update]
+
   def index
     @items = Item.all.order('created_at DESC')
   end
@@ -18,15 +20,26 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @item = Item.find(params[:id])
   end
 
   def edit
-    @item = Item.find(params[:id])
   end
+
+  def update
+    if @item.save
+      redirect_to item_path(@item.id)
+    else
+      render edit_item_path(@item.id)
+    end  
+  end 
+
   private
 
   def item_params
     params.require(:item).permit(:image, :name, :explanation, :category_id, :condition_id, :delivery_id, :prefecture_id, :delivery_day_id, :price).merge(user_id: current_user.id)
+  end
+
+  def set_item
+    @item = Item.find(params[:id])
   end
 end
