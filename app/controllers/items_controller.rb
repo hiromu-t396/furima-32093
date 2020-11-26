3291bb44c6e071bf2e6d82ce31e7b777009b8a44
hiromu-t396 <hiromu1193966@gmail.com>
2020-11-26 12:29:22 +0900

    controllerに投稿者のみeditページに遷移できる記述

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,5 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, only: [:new]
+  before_action :authenticate_user!, except: [:index, :show]
   before_action :set_item, only: [:show, :edit, :update]
 
   def index
@@ -23,6 +23,9 @@ class ItemsController < ApplicationController
   end
 
   def edit
+    unless @item.user_id == current_user.id
+      redirect_to action: :index
+    end
   end
 
   def update
