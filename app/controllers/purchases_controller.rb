def29c23c570dc848536a3b4757ca8c1c4158904
hiromu-t396 <hiromu1193966@gmail.com>
2020-11-29 15:44:11 +0900

    purchaseコントローラー、モデル作成

---
+++ app/controllers/purchases_controller.rb
@@ -0,0 +1,29 @@
+class PurchasesController < ApplicationController
+  def index
+    @item = Item.find(params[:item_id])
+    @item_purchase = ItemPurchase.new
+  end
+
+  def new
+  end
+
+  def create
+    # binding.pry
+    @item = Item.find(params[:item_id])
+    @item_purchase = ItemPurchase.new(purchase_params)
+    if @item_purchase.valid?
+      @item_purchase.save
+      redirect_to items_path
+    else  
+      render action: :index
+    end
+  end
+
+  private
+  def item
+    item = Item.find(params[:item_id])
+  end
+  def purchase_params
+    params.require(:item_purchase).permit(:postal_code, :prefecture_id, :municipality, :address, :building_name, :phone_number).merge(user_id: current_user.id, item_id: item.id)
+  end
+end
