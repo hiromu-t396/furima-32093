4228787dc3c219a41aeef074400e6c07d4a22075
hiromu-t396 <hiromu1193966@gmail.com>
2020-11-30 23:11:55 +0900

    エラーメッセージ記述

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,17 +3,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :email, uniqueness: true
-  validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i }
-
-  with_options format: { with: /\A[ぁ-んァ-ン一-龥]+\z/ } do
-    validates :lastname
-    validates :firstname
-  end
-
-  with_options format: { with: /\A[ァ-ン]+\z/ } do
-    validates :lastname_kana
-    validates :firstname_kana
-  end
+  validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i, message: "Include both letters and numbers"}
 
   with_options presence: true do
     validates :nickname
@@ -24,6 +14,16 @@ class User < ApplicationRecord
     validates :lastname_kana
     validates :firstname_kana
     validates :birth_day
+  end
+
+  with_options format: { with: /\A[ぁ-んァ-ン一-龥]+\z/, message: "Full-width characters"} do
+    validates :lastname
+    validates :firstname
+  end
+
+  with_options format: { with: /\A[ァ-ン]+\z/, message: "Full-width katakana characters" } do
+    validates :lastname_kana
+    validates :firstname_kana
   end
 
   has_many :items
