fd8f69d7adc4f6a9bfb19b904f9e8204659d2cde
hiromu-t396 <hiromu1193966@gmail.com>
2020-11-30 11:57:27 +0900

    サーバーサイドでトークンの情報を受け取る

--- app/controllers/purchases_controller.rb
+++ app/controllers/purchases_controller.rb
@@ -23,7 +23,8 @@ class PurchasesController < ApplicationController
   def item
     item = Item.find(params[:item_id])
   end
+
   def purchase_params
-    params.require(:item_purchase).permit(:postal_code, :prefecture_id, :municipality, :address, :building_name, :phone_number).merge(user_id: current_user.id, item_id: item.id)
+    params.require(:item_purchase).permit(:postal_code, :prefecture_id, :municipality, :address, :building_name, :phone_number).merge(user_id: current_user.id, item_id: item.id, token: params[:token])
   end
 end
